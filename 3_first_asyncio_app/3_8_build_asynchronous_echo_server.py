73ba8b5491df91cce99c739a2f7a96744b2acb4c
develop <dofastsite@gmail.com>
2024-05-06 22:36:47 +0300

    Update third chapter

--- 3_first_asyncio_app/3_8_build_asynchronous_echo_server.py
+++ 3_first_asyncio_app/3_8_build_asynchronous_echo_server.py
@@ -1,20 +1,32 @@
 import asyncio
+import logging
 import socket
 from asyncio import AbstractEventLoop
 
 
-async def echo(connection: socket,
-               loop: AbstractEventLoop) -> None:
+async def echo(
+        connection: socket,
+        loop: AbstractEventLoop
+) -> None:
     while data := await loop.sock_recv(connection, 1024):
-        await loop.sock_sendall(connection, data)
+        try:
+            if data == b'boom\r\n':
+                raise Exception("Неожиданная ошибка сети")
+            await loop.sock_sendall(connection, data)
+        except Exception as ex:
+            logging.exception(ex)
+        finally:
+            connection.close()
 
 
-async def listen_for_connection(server_socket: socket,
-                                loop: AbstractEventLoop):
+async def listen_for_connection(
+        server_socket: socket,
+        loop: AbstractEventLoop
+):
     while True:
         connection, address = await loop.sock_accept(server_socket)
         connection.setblocking(False)
-        print(f"Got a connection from {address}")
+        print(f"Получен запрос на подключение от {address}")
         asyncio.create_task(echo(connection, loop))
 
 
@@ -28,5 +40,6 @@ async def main():
     server_socket.listen()
 
     await listen_for_connection(server_socket, asyncio.get_event_loop())
+
 
 asyncio.run(main())
